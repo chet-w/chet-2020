6a90706fe6a130a685cc6de75ab24a0bed2536f7
Chet Wijesekera <chethana96@gmail.com>
2020-10-15 21:07:20 +1300

    Add fade in up animation to text

--- src/components/LandingSection/LandingSection.tsx
+++ src/components/LandingSection/LandingSection.tsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import { Section, PrimarySectionArea, SecondarySectionArea } from './styles';
 import { Accent } from "../Accent";
+import { motion } from 'framer-motion';
 
 export const LandingSection = () => (
   <Section>
@@ -12,8 +13,30 @@ export const LandingSection = () => (
 const PrimaryTitle = () => (
   <PrimarySectionArea>
     <h1>
-      Hi,<br/>
-      I'm <Accent>Chet</Accent>
+      <motion.span
+        style={{ display: "inline-block" }}
+        initial={{ opacity: 0, y: 50 }}
+        animate={{ opacity: 1, y: 0 }}
+      >
+        Hi,
+      </motion.span>
+      <br/>
+      <motion.span
+        style={{ display: "inline-block", marginRight: "1rem" }}
+        initial={{ opacity: 0, y: 50 }}
+        animate={{ opacity: 1, y: 0 }}
+        transition={{ delay: 0.8 }}
+      >
+        I'm 
+      </motion.span>
+      <motion.span
+        style={{ display: "inline-block" }}
+        initial={{ opacity: 0, y: 50 }}
+        animate={{ opacity: 1, y: 0 }}
+        transition={{ delay: 0.9 }}
+      >
+        <Accent>Chet</Accent>
+      </motion.span>
     </h1>
   </PrimarySectionArea>
 );
@@ -23,3 +46,5 @@ const SecondaryTitle = () => (
     <h3>I'm a <Accent>Front-end</Accent> Developer</h3>
   </SecondarySectionArea>
 );
+
+
