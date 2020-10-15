5a0096ccd7e74d3c1c1baeb45e9bc4f3f64465fc
Chet Wijesekera <chethana96@gmail.com>
2020-10-15 21:51:59 +1300

    Create about section component

---
+++ src/components/AboutSection/AboutSection.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import { Section } from '../LandingSection/styles';
+
+interface Props {
+  
+}
+
+export const AboutSection = (props: Props) => {
+  return (
+    <Section>
+      <h2>Section</h2>
+    </Section>
+  )
+};
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,10 +1,12 @@
 import React from "react";
 import { Layout } from "../components/layout";
 import { LandingSection } from "../components/LandingSection";
+import { AboutSection } from "../components/AboutSection";
 
 const App = () => (
   <Layout>
     <LandingSection />
+    <AboutSection />
   </Layout>
 );
 
