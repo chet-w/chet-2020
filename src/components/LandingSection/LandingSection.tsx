bdfde02831aacbf5e928ce022087ff7c7f8802ff
Chet Wijesekera <chethana96@gmail.com>
2020-10-15 20:18:27 +1300

    Create landing section component section

---
+++ src/components/LandingSection/LandingSection.tsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export const LandingSection = () => (
+  <section>
+    <h1>Hey, I'm Chet</h1>
+  </section>
+);
--- src/components/layout/layout.tsx
+++ src/components/layout/layout.tsx
@@ -4,11 +4,12 @@ import { theme, GlobalStyles } from "../../styles";
 import { Header } from "../Header";
 import { Container } from "../Container";
 
-const Layout: React.FC = () => (
+const Layout: React.FC = ({ children }) => (
   <ThemeProvider theme={theme()}>
+    <GlobalStyles />
     <Header />
     <Container>
-      <GlobalStyles />
+      { children }
     </Container>
   </ThemeProvider>
 );
