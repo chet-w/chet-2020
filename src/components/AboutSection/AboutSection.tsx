78621ce79e2e0119faf7b6f415d70091f0d50ee0
Chet Wijesekera <chethana96@gmail.com>
2020-10-17 00:10:29 +1300

    Add ref for the section

--- src/components/AboutSection/AboutSection.tsx
+++ src/components/AboutSection/AboutSection.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react'
+import React, { useState, useRef } from 'react'
 import { Section } from '../Section';
 import { Card } from './styles';
 import FadeInUp from "../Animations/FadeInUp"
@@ -10,9 +10,15 @@ interface Props {
 export const AboutSection = (props: Props) => {
 
   const [shouldAnimate, setShouldAnimate] = useState(false);
+  const AboutSectionRef = useRef(null);
+
 
   return (
-    <Section middled centered>
+    <Section
+      middled 
+      centered
+      ref={AboutSectionRef}
+    >
       <h2>I like making</h2>
       <button onClick={() => setShouldAnimate(!shouldAnimate)}>fade in </button>
       <FadeInUp
