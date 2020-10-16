a145257e7ab4640b05d1fb2282f3de13cff7b4b8
Chet Wijesekera <chethana96@gmail.com>
2020-10-17 02:35:48 +1300

    Update page title

--- src/components/layout/layout.tsx
+++ src/components/layout/layout.tsx
@@ -3,9 +3,11 @@ import { ThemeProvider } from "styled-components";
 import { theme, GlobalStyles } from "../../styles";
 import { Header } from "../Header";
 import { Container } from "../Container";
+import { SEO } from "../seo";
 
 const Layout: React.FC = ({ children }) => (
   <ThemeProvider theme={theme()}>
+    <SEO title="Chet | Front-end Developer" />
     <GlobalStyles />
     <Header />
     <main>
