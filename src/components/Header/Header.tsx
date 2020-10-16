52952431aefbcaf95465bb57c8bd04e428653db3
Chet Wijesekera <chethana96@gmail.com>
2020-10-17 02:04:33 +1300

    Add header links

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,11 +1,21 @@
 import React from 'react'
-import { StyledHeader } from './styles';
+import { StyledHeader, StyledHeaderLinks } from './styles';
 import { Container } from "../Container";
+import { Accent } from '../Accent';
 
 export const Header = () => (
   <StyledHeader>
     <Container>
-      Header title
+      <h3><Accent>Chet</Accent></h3>
+      <HeaderLinks />
     </Container>
   </StyledHeader>
 );
+
+const HeaderLinks = () => (
+  <StyledHeaderLinks>
+    <li>Ramblings</li>
+    <li>Github</li>
+    <li>Contact</li>
+  </StyledHeaderLinks>
+)
--- src/components/Header/styles.ts
+++ src/components/Header/styles.ts
@@ -1,13 +1,32 @@
 import styled from "styled-components";
 import { IGlobalStyles } from "../../@types/theme";
+import { Container } from "../Container";
 
 export const StyledHeader = styled.header`
   height: 5rem;
-  background: ${(props: IGlobalStyles) => props.theme.colors.light.neutrals.light};
+  background: ${(props: IGlobalStyles) => props.theme.colors.light.neutrals.lighter};
   position: fixed;
   left: 0;
   top: 0;
   width: 100vw;
   display: flex;
   align-items: center;
+  box-shadow: 0 0 20px 0 rgba(0, 0, 0, 0.1);
+
+  ${Container} {
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+    width: 100%;
+  }
+`;
+
+export const StyledHeaderLinks = styled.ul`
+  display: flex;
+  list-style: none;
+  padding: 0;
+
+  li {
+    margin: 0 1rem
+  }
 `;
